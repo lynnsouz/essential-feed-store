7762c99899faab8343359cd500b4582a030f6616
Lynneker Souza <lynnsouz@gmail.com>
2023-06-29 15:12:49 -0300

    Add local Models for CoreData insertion and retrieve.

--- FeedStoreChallenge/Model/ManagedCache.swift
+++ FeedStoreChallenge/Model/ManagedCache.swift
@@ -1,5 +1,21 @@
-//	
-// Copyright © Essential Developer. All rights reserved.
-//
+import CoreData
 
-import Foundation
+@objc(ManagedCache)
+internal class ManagedCache: NSManagedObject {
+	@NSManaged var timestamp: Date
+	@NSManaged var feed: NSOrderedSet
+}
+
+extension ManagedCache {
+	static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+		guard let entityName = entity().name else { return nil }
+		let request = NSFetchRequest<ManagedCache>(entityName: entityName)
+		request.returnsObjectsAsFaults = false
+		return try context.fetch(request).first
+	}
+
+	static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+		try find(in: context).map(context.delete)
+		return ManagedCache(context: context)
+	}
+}
